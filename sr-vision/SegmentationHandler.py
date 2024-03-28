585dd76370537090d45a38dc70e95bf468a10937
alvister88 <ayzhu2014@gmail.com>
2024-03-27 18:55:48 -0700

    Added display function to show segementation polygon, bbox, class and confidence

--- sr-vision/SegmentationHandler.py
+++ sr-vision/SegmentationHandler.py
@@ -29,8 +29,33 @@ class SegmentationHandler(SegmentationHandlerBase):
         self.bbox = None
         self.mask = None
         
-        
+    '''Getters:'''
+    def get_bbox(self):
+        return self.bbox
+    
+    def get_mask(self):
+        return self.mask
+    
+    def get_positions(self):
+        return self.positions
+    
+    def get_frame(self):
+        return self.frame
+     
     def get_depth_at_centroid_seg(self, polygon):
+        """
+        Calculate the centroid of the given polygon and retrieve the depth value at the centroid. 
+
+        Parameters:
+        polygon (list): List of points representing the polygon.
+
+        Returns:
+        float or None: Depth value at the centroid, or None if no depth value is available.
+        float: X coordinate of the centroid in camera coordinates.
+        float: Y coordinate of the centroid in camera coordinates.
+        int: X pixel coordinate of the centroid in the depth frame.
+        int: Y pixel coordinate of the centroid in the depth frame.
+        """
         # Calculate the centroid
         M = cv2.moments(np.array(polygon, dtype=np.int32))
         if M["m00"] != 0:
@@ -57,6 +82,65 @@ class SegmentationHandler(SegmentationHandlerBase):
                 return None, 0, 0, 0, 0
         else:
             return None, 0, 0, 0, 0
+        
+    '''Setters:'''
+
+    def set_frame(self, frame):
+        self.frame = frame
+        
+    def set_point_cloud(self, point_cloud):
+        self.point_cloud = point_cloud
+        
+    def set_display(self, display):
+        self.display = display
+    
+    '''Display Functions:'''
+ 
+    def display_data(self, box, polygon, bbox, ID, center_x, center_y):
+        """
+        A function to display data including bounding box, segmentation polygon, and label on the frame.
+
+        Parameters:
+        - box: The bounding box object
+        - polygon: The segmentation polygon points
+        - bbox: The bounding box coordinates
+        - ID: The class ID
+        - center_x: The x-coordinate of the centroid
+        - center_y: The y-coordinate of the centroid
+
+        Returns:
+        This function applies the changes to the frame and does not return anything.
+        """
+        # get confidence and class name
+        confidence = box.conf[0]
+        current_class_name = self.classes[ID]
+        
+        # Draw bounding box 
+        x1, y1, x2, y2 = bbox
+        cv2.rectangle(self.frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        xyz_label = f"X: {self.center_3d[0]:.2f}, Y: {self.center_3d[1]:.2f}, Z: {self.center_3d[2]:.2f}"
+        label = f"{current_class_name} {confidence:.2f} ({xyz_label})"
+        
+        # Draw segmentation polygon
+        cv2.polylines(self.frame, [np.array(polygon, dtype=np.int32)], isClosed=True, color=(255, 0, 255), thickness=2)
+        
+        # Draw a circle at the 2D centroid
+        cv2.circle(self.frame, (center_x, center_y), 5, (0, 0, 255), -1)
+        (label_width, label_height), baseline = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+
+        # Adjust the y position of the label to sit on top or inside the bounding box
+        # label_y_position = max(y1, label_height + baseline) 
+        label_y_position = y1 - label_height + baseline + 4
+        box_y_position = y1 - label_height - baseline 
+        if (box_y_position < 0): 
+            label_y_position = y1 + label_height - baseline + 6 
+            box_y_position = y1 + label_height + baseline                      
+
+        # Create a rectangle for the label background and put the text directly on the bounding box
+        cv2.rectangle(self.frame, (x1, box_y_position), (x1 + label_width, y1), (0, 255, 0), cv2.FILLED)
+        cv2.putText(self.frame, label, (x1, label_y_position), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    
+    '''Processers:'''
     
     def process_model(self):
         self.results = self.model(self.frame, imgsz=(self.max_model_size), stream=True, conf=self.det_conf)
@@ -79,13 +163,20 @@ class SegmentationHandler(SegmentationHandlerBase):
                 bbox = box.xyxy[0].cpu().numpy().astype(int)
                 # Extract bounding box classification
                 cls = int(box.cls[0].item())
-                # Extract confidence level
-                confidence = box.cpu().conf[0]
                 
                 # Extract segmentation mask
                 polygon = mask.xy[0]
                 
-                # 
+                # Extract depth at centroid
+                depth, x, y, cx, cy = self.get_depth_at_centroid_seg(polygon)
+                position = np.array([[cls, x, y, depth]])
+                # Append position of detection to 2D Matrix
+                self.positions = np.vstack((self.positions, position))
+                
+                if (self.display):
+                    # display data on frame
+                    self.display_data(box, polygon, bbox, cls, center_x, center_y)
+                
                 
                 
                 
